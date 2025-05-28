d36c963bd9fe430ab0f3471b2cfb0f1deecbb0cb
abbesm0hamed <abbesmohamed717@gmail.com>
2025-05-28 09:19:33 +0100

    fix(storefront): folder structure but still have errors rendering payload created pages...

--- apps/storefront/src/app/(frontend)/[locale]/[countryCode]/robots.ts
+++ apps/storefront/src/app/(frontend)/[locale]/[countryCode]/robots.ts
@@ -1,10 +1,9 @@
-import { env } from 'env';
-import { MetadataRoute } from 'next';
+import type { MetadataRoute } from 'next';
 
-const protocol = env.NEXT_PUBLIC_WWW_URL?.startsWith('https')
+const protocol = process.env.NEXT_PUBLIC_WWW_URL?.startsWith('https')
   ? 'https'
   : 'http';
-const url = new URL(`${protocol}://${env.NEXT_PUBLIC_WWW_URL}`);
+const url = new URL(`${protocol}://${process.env.NEXT_PUBLIC_WWW_URL}`);
 
 export default function robots(): MetadataRoute.Robots {
   return {
