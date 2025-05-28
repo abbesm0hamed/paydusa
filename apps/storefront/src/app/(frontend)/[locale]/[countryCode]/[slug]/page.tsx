37b27aca8b3fe892399ca359dc37ff463a125bee
abbesm0hamed <abbesmohamed717@gmail.com>
2025-05-28 18:43:23 +0100

    fix(storefront): tailwindcss config

--- apps/storefront/src/app/(frontend)/[locale]/[countryCode]/[slug]/page.tsx
+++ apps/storefront/src/app/(frontend)/[locale]/[countryCode]/[slug]/page.tsx
@@ -15,6 +15,7 @@ import { generateMeta } from '@/utilities/generateMeta';
 import PageClient from './page.client';
 import { LivePreviewListener } from '@/components/LivePreviewListener';
 
+// FIX: SSG
 // export async function generateStaticParams() {
 //   console.log('🔍 generateStaticParams called for [slug] page');
 //
