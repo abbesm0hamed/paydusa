e33ed7e978084743bd1b5248b0aef90f9e29bbf3
abbesm0hamed <abbesmohamed717@gmail.com>
2025-09-27 19:36:15 +0100

    feat(medusa): sentry integration

--- apps/medusa/src/api/middlewares.ts
+++ apps/medusa/src/api/middlewares.ts
@@ -2,10 +2,27 @@ import {
   defineMiddlewares,
   validateAndTransformBody,
   authenticate,
+  errorHandler,
+  MedusaNextFunction,
+  MedusaRequest,
+  MedusaResponse,
 } from "@medusajs/framework/http"
+import { MedusaError } from "@medusajs/framework/utils"
+import * as Sentry from "@sentry/node"
 import { SearchSchema } from "./store/products/search/route"
 
+const originalErrorHandler = errorHandler()
+
 export default defineMiddlewares({
+  errorHandler: (
+    error: MedusaError | any, 
+    req: MedusaRequest, 
+    res: MedusaResponse, 
+    next: MedusaNextFunction
+  ) => {
+    Sentry.captureException(error)
+    return originalErrorHandler(error, req, res, next)
+  },
   routes: [
     {
       matcher: "/store/products/search",
---
+++ apps/medusa/src/api/test-sentry/route.ts
@@ -0,0 +1,12 @@
+import { MedusaRequest, MedusaResponse } from "@medusajs/framework/http"
+import { MedusaError } from "@medusajs/framework/utils"
+
+export async function GET(
+  req: MedusaRequest,
+  res: MedusaResponse
+) {
+  throw new MedusaError(
+    MedusaError.Types.UNEXPECTED_STATE,
+    "This is a test error for Sentry integration."
+  )
+}
