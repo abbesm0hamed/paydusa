69b2f18104a09893c09b78736f324af4dd2e5c92
abbesm0hamed <abbesmohamed717@gmail.com>
2025-10-04 15:20:59 +0100

    feat: remove google auth

--- apps/medusa/medusa-config.ts
+++ apps/medusa/medusa-config.ts
@@ -41,15 +41,6 @@ module.exports = defineConfig({
             resolve: "@medusajs/medusa/auth-emailpass",
             id: "emailpass",
           },
-          {
-            resolve: "@medusajs/medusa/auth-google",
-            id: "google",
-            options: {
-              clientId: process.env.GOOGLE_CLIENT_ID,
-              clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-              callbackUrl: process.env.GOOGLE_CALLBACK_URL,
-            },
-          },
         ],
       },
 
